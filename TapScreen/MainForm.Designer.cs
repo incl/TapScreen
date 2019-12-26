7bba3e4b01d4bfc579b637eb268b590f58fcc735
incl <lee.incl@gmail.com>
2019-12-27 00:38:40 +0900

    First commit

---
+++ TapScreen/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TapScreen
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.screen = new System.Windows.Forms.PictureBox();
+            this.numSensability = new System.Windows.Forms.NumericUpDown();
+            this.btnx1 = new System.Windows.Forms.Button();
+            this.btnx05 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSensability)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 39);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(12, 69);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // screen
+            // 
+            this.screen.BackColor = System.Drawing.Color.White;
+            this.screen.Location = new System.Drawing.Point(95, 13);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(693, 425);
+            this.screen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.screen.TabIndex = 2;
+            this.screen.TabStop = false;
+            this.screen.Click += new System.EventHandler(this.Screen_Click);
+            // 
+            // numSensability
+            // 
+            this.numSensability.Location = new System.Drawing.Point(13, 123);
+            this.numSensability.Name = "numSensability";
+            this.numSensability.Size = new System.Drawing.Size(75, 21);
+            this.numSensability.TabIndex = 3;
+            this.numSensability.ValueChanged += new System.EventHandler(this.Sensability_Changed);
+            // 
+            // btnx1
+            // 
+            this.btnx1.Location = new System.Drawing.Point(13, 202);
+            this.btnx1.Name = "btnx1";
+            this.btnx1.Size = new System.Drawing.Size(45, 23);
+            this.btnx1.TabIndex = 4;
+            this.btnx1.Text = "x1";
+            this.btnx1.UseVisualStyleBackColor = true;
+            this.btnx1.Click += new System.EventHandler(this.Sizex1_Click);
+            // 
+            // btnx05
+            // 
+            this.btnx05.Location = new System.Drawing.Point(12, 173);
+            this.btnx05.Name = "btnx05";
+            this.btnx05.Size = new System.Drawing.Size(45, 23);
+            this.btnx05.TabIndex = 5;
+            this.btnx05.Text = "x 0.5";
+            this.btnx05.UseVisualStyleBackColor = true;
+            this.btnx05.Click += new System.EventHandler(this.SizeX05_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sensability";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Screen Size";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(13, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(72, 21);
+            this.txtName.TabIndex = 8;
+            this.txtName.Text = "temp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 562);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnx05);
+            this.Controls.Add(this.btnx1);
+            this.Controls.Add(this.numSensability);
+            this.Controls.Add(this.screen);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "TapScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSensability)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox screen;
+        private System.Windows.Forms.NumericUpDown numSensability;
+        private System.Windows.Forms.Button btnx1;
+        private System.Windows.Forms.Button btnx05;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ TapScreen/MainForm.cs
@@ -0,0 +1,176 @@
+﻿using AForge.Imaging;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TapScreen
+{
+    public partial class MainForm : Form
+    {
+        Thread thread;
+
+        public float ScreenScale
+        {
+            get
+            {
+                return 1.0f;
+                // return btnx05.Enabled ? 1.0f : 0.5f;
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            btnStop.Enabled = false;
+            numSensability.Value = 80;
+            btnx05.Enabled = false;
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            btnPlay.Enabled = false;
+            btnStop.Enabled = true;
+
+            thread = new Thread(new ThreadStart(ThreadDisplay));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void ThreadDisplay()
+        {
+            var adb = new Process();
+            adb.StartInfo.FileName = "adb.exe";
+            adb.StartInfo.UseShellExecute = false;
+            adb.StartInfo.CreateNoWindow = true;
+            adb.StartInfo.RedirectStandardError = true;
+            adb.StartInfo.RedirectStandardInput = true;
+            adb.StartInfo.RedirectStandardOutput = true;
+
+            try
+            {
+                while (true)
+                {
+                    adb.StartInfo.Arguments = "shell screencap sdcard/screen.png";
+                    adb.Start();
+                    adb.WaitForExit();
+
+                    adb.StartInfo.Arguments = "pull sdcard/screen.png";
+                    adb.Start();
+                    adb.WaitForExit();
+
+                    using (var fs = new System.IO.FileStream("screen.png", System.IO.FileMode.Open))
+                    {
+                        var bitmap = new Bitmap(fs);
+                        screen.BeginInvoke(new Action(() => screen.Image = bitmap));
+
+                        var files = System.IO.Directory.GetFiles(string.Format("{0}/", txtName.Text));
+                        foreach (var file in files)
+                        {
+                            var saved = new Bitmap(file);
+                            float sensability = (float)numSensability.Value * 0.01f;
+                            if (CompareImages(bitmap, saved, sensability, 0.9f))
+                            {
+                                var pos = System.IO.Path.GetFileNameWithoutExtension(file);
+                                Tap(pos);
+                            }
+                        }
+                    }
+
+                    Thread.Sleep(3000);
+                }
+            }
+            catch (Exception e)
+            {
+                adb.Close();
+                adb.Dispose();
+
+                btnPlay.BeginInvoke(new Action(() => btnPlay.Enabled = true));
+                btnStop.BeginInvoke(new Action(() => btnStop.Enabled = false));
+
+                MessageBox.Show(e.ToString());
+
+                if (!(e is ThreadAbortException))
+                    Play_Click(null, null);
+            }
+        }
+
+        public Boolean CompareImages(Bitmap bmp1, Bitmap bmp2, double compareLevel, float similarityThreshold)
+        {
+            var newBitmap1 = ChangePixelFormat(new Bitmap(bmp1), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            var newBitmap2 = ChangePixelFormat(new Bitmap(bmp2), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            var tm = new ExhaustiveTemplateMatching(similarityThreshold);
+            var results = tm.ProcessImage(newBitmap1, newBitmap2);
+            if (results.Length <= 0)
+            {
+                return false;
+            }
+            var match = results[0].Similarity >= compareLevel;
+            return match;
+        }
+
+        private Bitmap ChangePixelFormat(Bitmap inputImage, System.Drawing.Imaging.PixelFormat newFormat)
+        {
+            return (inputImage.Clone(new Rectangle(0, 0, inputImage.Width, inputImage.Height), newFormat));
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            btnPlay.Enabled = true;
+            btnStop.Enabled = false;
+
+            thread.Abort();
+            thread = null;
+        }
+
+        private void Sensability_Changed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SizeX05_Click(object sender, EventArgs e)
+        {
+            btnx05.Enabled = false;
+            btnx1.Enabled = true;
+        }
+
+        private void Sizex1_Click(object sender, EventArgs e)
+        {
+            btnx05.Enabled = true;
+            btnx1.Enabled = false;
+        }
+
+        private void Screen_Click(object sender, EventArgs e)
+        {
+            var me = e as MouseEventArgs;
+
+            if (!System.IO.Directory.Exists(txtName.Text))
+                System.IO.Directory.CreateDirectory(txtName.Text);
+
+            var pos = $"{(int)(me.X / ScreenScale)} {(int)(me.Y / ScreenScale)}";
+            var bmp = new Bitmap(screen.Image);
+            bmp.Save(string.Format("{0}/{1}.png", txtName.Text, pos));
+            Tap(pos);
+        }
+
+        private void Tap(string pos)
+        {
+            var adb = new Process();
+            adb.StartInfo.FileName = "adb.exe";
+            adb.StartInfo.UseShellExecute = false;
+            adb.StartInfo.CreateNoWindow = true;
+            adb.StartInfo.RedirectStandardError = true;
+            adb.StartInfo.RedirectStandardInput = true;
+            adb.StartInfo.RedirectStandardOutput = true;
+
+            adb.StartInfo.Arguments = string.Format("shell input touchscreen tap {0}", pos);
+
+            adb.Start();
+            adb.WaitForExit();
+            adb.Close();
+            adb.Dispose();
+        }
+    }
+}
